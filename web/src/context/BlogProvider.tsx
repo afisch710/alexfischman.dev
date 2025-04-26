448dcf82e0a98df169b7f8e2e1af9ab48d05451b
Alex Fischman <afischman710@gmail.com>
2025-04-26 16:05:02 -0500

    Fixing yarn type-check failure

--- web/src/context/BlogProvider.tsx
+++ web/src/context/BlogProvider.tsx
@@ -1,6 +1,6 @@
 // /src/context/BlogProvider.tsx
 import React, { createContext, useContext, useEffect, useState } from "react";
-import { fetchBlogs } from "@/lib";
+import { fetchBlogs } from "@/lib/fetchBlogs";
 
 export interface Post {
   title: string;
--- web/src/pages/blog/[slug].tsx
+++ web/src/pages/blog/[slug].tsx
@@ -4,7 +4,7 @@ import { GetStaticPaths, GetStaticProps } from 'next';
 import { Container } from '@mui/material';
 import BlogPost from '../../components/blog/BlogPost';
 import { Post } from '../../context/BlogProvider';
-import { fetchBlogs } from '@/lib';
+import { fetchBlogs } from '@/lib/fetchBlogs';
 
 interface BlogPostPageProps {
   post: Post;
