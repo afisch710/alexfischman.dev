c0407b7903d0a194d1509bff1c87b315ef7d0d82
Alex Fischman <afischman710@gmail.com>
2025-04-30 13:08:24 -0500

    Configuring netlify for github backend, updating script to use correct fields

--- web/scripts/generate-posts.ts
+++ web/scripts/generate-posts.ts
@@ -6,18 +6,24 @@ const matter = require("gray-matter");
 const postsDirectory = path.join(process.cwd(), "..", "content", "blog");
 const filenames = fs.readdirSync(postsDirectory);
 
-const posts = filenames.map((filename) => {
-  const filePath = path.join(postsDirectory, filename);
-  const fileContents = fs.readFileSync(filePath, "utf8");
-  const { data, content } = matter(fileContents);
-  return {
-    title: data.title,
-    date: data.date,
-    description: data.description,
-    slug: filename.replace(/\.md$/, ""),
-    body: content,
-  };
-}).sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
+const posts = filenames
+  .map((filename) => {
+    const filePath = path.join(postsDirectory, filename);
+    const fileContents = fs.readFileSync(filePath, "utf8");
+    const { data, content } = matter(fileContents);
+    return {
+      title: data.title,
+      date: data.date,
+      description: data.description,
+      slug: filename.replace(/\.md$/, ""),
+      body: content,
+      ogImage: data.thumbnail || null,
+      tags: data.tags || [],
+      draft: data.draft || false,
+    };
+  })
+  .filter((post) => !post.draft)
+  .sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
 
 fs.writeFileSync(
   path.join(process.cwd(), "src", "data", "posts.json"),
